73955c1067b79e734dece7e875652f819c565178
Chigoziem Nwaiwu <princegoziem@gmail.com>
2025-09-24 12:39:00 +0100

    Update Nuxt.js configuration to set base URL to '/wish-weaver/' for improved deployment structure.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
         lang: 'en',
       },
     },
-    baseURL: 'wish-weaver',
+    baseURL: '/wish-weaver/',
     buildAssetsDir: 'assets/',
   },
   compatibilityDate: '2025-07-15',
