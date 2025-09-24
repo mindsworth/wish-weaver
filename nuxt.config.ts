ff7312ef9534185043f89d693b499f6e2db46a57
Chigoziem Nwaiwu <princegoziem@gmail.com>
2025-09-24 11:46:24 +0100

    Update Nuxt.js configuration to change CSS file path and add main CSS file with Tailwind CSS variables and utilities.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/main.css'],
+  css: ['~/assets/css/main.css'],
   components: [
     {
       path: '~/components',
